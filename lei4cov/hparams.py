e9385ff5786c435b3762d85e01069dcbca43e9fc
hitales-Jenkins使用 <46097539+hitales-jenkins@users.noreply.github.com>
2020-09-28 18:18:37 +0800

    Update hparams.py
    updated annotation

--- lei4cov/hparams.py
+++ lei4cov/hparams.py
@@ -6,7 +6,7 @@ import argparse
 
 class Hparams:
     parser = argparse.ArgumentParser()
-    # input
+    # inputing
     parser.add_argument('--n_input', default=16, type=int)
     parser.add_argument('--maxlen_seqs', default=6, type=int, help="max length of input event")
     # model
@@ -15,7 +15,7 @@ class Hparams:
     parser.add_argument('--num_blocks', default=1, type=int, help="number of blocks")
     parser.add_argument('--num_heads', default=8, type=int, help="head number of the multi-head attenion")
     parser.add_argument('--dropout_rate', default=0.1, type=float, help="dropout rate")
-    # train
+    # training
     parser.add_argument('--max_epoch', default=10, type=int)
     parser.add_argument('--batch_size', default=8, type=int)
     parser.add_argument('--display_step', default=1, type=int, help='display frequency of the training process')
